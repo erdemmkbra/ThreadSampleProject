4287cf59f9e6759fa84ea989bc8e358c25200365
kbraerdem <108875163+kbraerdem@users.noreply.github.com>
2022-11-12 16:22:08 +0300

    Initial commit

---
+++ ThreadSample/MyTimer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSample
+{
+    public delegate int MyTimerTickHandler(int x, string s);
+    public class MyTimer
+    {
+        public int Interval { get; set; } = 100;
+
+        Thread myThread;
+        bool timerStarter = false;
+
+        public event MyTimerTickHandler MyTimerTick = null;
+
+        public MyTimer()
+        {
+
+        }
+
+        private void MyWorker()
+        {
+            while (timerStarter)
+            {
+                try
+                {
+                    if(MyTimerTick !=null)
+                        MyTimerTick(DateTime.Now.Second,  "M: " + DateTime.Now.Millisecond);
+
+                }
+                catch { }
+
+                try
+                {
+                    Thread.Sleep(Interval);
+                }
+                catch { }
+
+            }
+            
+
+        }
+
+        public void Start()
+        {
+            timerStarter = true;
+            myThread = new Thread(new ThreadStart(MyWorker));
+            myThread.Start();
+
+        }
+
+        public void Stop()
+        {
+            timerStarter= false;
+            myThread.Abort();
+            myThread = null;
+
+
+        }
+
+    }
+}
---
+++ ThreadSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int interval = 100;
+
+            int temp = 0;
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter interval :  ");
+                interval = int.Parse(Console.ReadLine());
+                if (interval == 999) break;
+
+                MyTimer timer = new MyTimer();
+                timer.Interval = interval;
+                timer.MyTimerTick += Timer_MyTimerTick;
+                timer.MyTimerTick += Timer_MyTimerTick;
+
+                //mytimer2  console progress bar
+
+                timer.Start();
+
+                Console.ReadLine();
+                timer.Stop();
+
+                Console.WriteLine("Timer Stopped!");
+                Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
+
+        private static int Timer_MyTimerTick(int x, string s)
+        {
+            Console.WriteLine(x + "-" + s);
+
+            return 0;
+        }
+    }
+}
